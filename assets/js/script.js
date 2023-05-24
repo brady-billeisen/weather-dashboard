55b3c44c47eb7b521e1eb13f3614c676648a5dc1
brady <bradybill36@gmail.com>
2023-05-24 00:33:23 -0500

    got my functions working properly, finally and now I'm gonna start working on getting it to display on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,43 @@
 var apiKey = 'cf33cf9f2c94399c3184c97387dc10aa';
-var searchHistory = [];
+var searchForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#search-input');
+var cardTitleEl = document.querySelector('.card-title')
+var currentDate = dayjs().format('MM/DD/YYYY');
 
-function fetchWeatherData(city) {
-    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
-    console.log('API URL', apiUrl);
-    fetch(apiUrl).then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        console.log('DATA', data);
-    });
+
+function getGeo(city) {
+    var geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
+    fetch(geoUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data[0]);
+            getWeather(data[0].lat, data[0].lon);
+        });
 }
+
+
+function getWeather(lat, lon) {
+    var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+    fetch(weatherUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        });
+}
+
+
+searchForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var city = searchInput.value.trim();
+    cardTitleEl.innerHTML = city + ' ' + '(' + currentDate + ')';
+    console.log(city);
+    getGeo(city);
+})
+
+
+// getGeo('Nashville');
