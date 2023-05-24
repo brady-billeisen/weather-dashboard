f2fc8e91eb4c5d75f9809100346d04e3de306f74
brady <bradybill36@gmail.com>
2023-05-24 01:38:25 -0500

    got everything displaying properly for the current day/weather. Tomorrow I'm gonna work on the 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 var apiKey = 'cf33cf9f2c94399c3184c97387dc10aa';
 var searchForm = document.querySelector('#search-form');
 var searchInput = document.querySelector('#search-input');
-var cardTitleEl = document.querySelector('.card-title')
+var cardTitleEl = document.querySelector('.card-title');
+var tempEl = document.querySelector('#temp');
+var windEl = document.querySelector('#wind');
+var humidityEl = document.querySelector('#humidity');
 var currentDate = dayjs().format('MM/DD/YYYY');
 
 
@@ -13,12 +16,12 @@ function getGeo(city) {
         })
         .then(function(data) {
             console.log(data[0]);
-            getWeather(data[0].lat, data[0].lon);
+            getWeather(data[0].lat, data[0].lon, city);
         });
 }
 
 
-function getWeather(lat, lon) {
+function getWeather(lat, lon, city) {
     var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
     fetch(weatherUrl)
         .then(function(response) {
@@ -26,6 +29,12 @@ function getWeather(lat, lon) {
         })
         .then(function(data) {
             console.log(data);
+            var iconUrl = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
+            cardTitleEl.innerHTML = city + ' ' + '(' + currentDate + ')';
+            cardTitleEl.innerHTML += `<img src="${iconUrl}" alt="Weather Icon">`;
+            tempEl.innerHTML = 'Temp: ' + Math.floor(data.main.temp) + '°F';
+            windEl.innerHTML = 'Wind: ' + Math.floor(data.wind.speed) + ' MPH';
+            humidityEl.innerHTML = 'Humidity: ' + Math.floor(data.main.humidity) + '%';
         });
 }
 
